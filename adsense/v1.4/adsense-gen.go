e7e6a9ce96f7f9405f81562d8445f1a61ad1f3a1
Nguan <nguansak@gmail.com>
2015-07-10 15:42:50 +0700

    Fix for multiple dimension and metric and other repeated parameters
    adsense.accounts.reports.generate
    parameters: dimension, filter, metric, sort
    
    adsense.reports.generate
    parameters: accountId, dimension, filter, metric, sort

--- adsense/v1.4/adsense-gen.go
+++ adsense/v1.4/adsense-gen.go
@@ -2348,14 +2348,14 @@ func (c *AccountsReportsGenerateCall) Currency(currency string) *AccountsReports
 
 // Dimension sets the optional parameter "dimension": Dimensions to base
 // the report on.
-func (c *AccountsReportsGenerateCall) Dimension(dimension string) *AccountsReportsGenerateCall {
+func (c *AccountsReportsGenerateCall) Dimension(dimension []string) *AccountsReportsGenerateCall {
 	c.opt_["dimension"] = dimension
 	return c
 }
 
 // Filter sets the optional parameter "filter": Filters to be run on the
 // report.
-func (c *AccountsReportsGenerateCall) Filter(filter string) *AccountsReportsGenerateCall {
+func (c *AccountsReportsGenerateCall) Filter(filter []string) *AccountsReportsGenerateCall {
 	c.opt_["filter"] = filter
 	return c
 }
@@ -2377,7 +2377,7 @@ func (c *AccountsReportsGenerateCall) MaxResults(maxResults int64) *AccountsRepo
 
 // Metric sets the optional parameter "metric": Numeric columns to
 // include in the report.
-func (c *AccountsReportsGenerateCall) Metric(metric string) *AccountsReportsGenerateCall {
+func (c *AccountsReportsGenerateCall) Metric(metric []string) *AccountsReportsGenerateCall {
 	c.opt_["metric"] = metric
 	return c
 }
@@ -2386,7 +2386,7 @@ func (c *AccountsReportsGenerateCall) Metric(metric string) *AccountsReportsGene
 // metric to sort the resulting report on, optionally prefixed with "+"
 // to sort ascending or "-" to sort descending. If no prefix is
 // specified, the column is sorted ascending.
-func (c *AccountsReportsGenerateCall) Sort(sort string) *AccountsReportsGenerateCall {
+func (c *AccountsReportsGenerateCall) Sort(sort []string) *AccountsReportsGenerateCall {
 	c.opt_["sort"] = sort
 	return c
 }
@@ -2425,10 +2425,14 @@ func (c *AccountsReportsGenerateCall) Do() (*AdsenseReportsGenerateResponse, err
 		params.Set("currency", fmt.Sprintf("%v", v))
 	}
 	if v, ok := c.opt_["dimension"]; ok {
-		params.Set("dimension", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("dimension", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["filter"]; ok {
-		params.Set("filter", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("filter", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["locale"]; ok {
 		params.Set("locale", fmt.Sprintf("%v", v))
@@ -2437,10 +2441,14 @@ func (c *AccountsReportsGenerateCall) Do() (*AdsenseReportsGenerateResponse, err
 		params.Set("maxResults", fmt.Sprintf("%v", v))
 	}
 	if v, ok := c.opt_["metric"]; ok {
-		params.Set("metric", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("metric", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["sort"]; ok {
-		params.Set("sort", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("sort", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["startIndex"]; ok {
 		params.Set("startIndex", fmt.Sprintf("%v", v))
@@ -4375,7 +4383,7 @@ func (r *ReportsService) Generate(startDate string, endDate string) *ReportsGene
 
 // AccountId sets the optional parameter "accountId": Accounts upon
 // which to report.
-func (c *ReportsGenerateCall) AccountId(accountId string) *ReportsGenerateCall {
+func (c *ReportsGenerateCall) AccountId(accountId []string) *ReportsGenerateCall {
 	c.opt_["accountId"] = accountId
 	return c
 }
@@ -4390,14 +4398,14 @@ func (c *ReportsGenerateCall) Currency(currency string) *ReportsGenerateCall {
 
 // Dimension sets the optional parameter "dimension": Dimensions to base
 // the report on.
-func (c *ReportsGenerateCall) Dimension(dimension string) *ReportsGenerateCall {
+func (c *ReportsGenerateCall) Dimension(dimension []string) *ReportsGenerateCall {
 	c.opt_["dimension"] = dimension
 	return c
 }
 
 // Filter sets the optional parameter "filter": Filters to be run on the
 // report.
-func (c *ReportsGenerateCall) Filter(filter string) *ReportsGenerateCall {
+func (c *ReportsGenerateCall) Filter(filter []string) *ReportsGenerateCall {
 	c.opt_["filter"] = filter
 	return c
 }
@@ -4419,7 +4427,7 @@ func (c *ReportsGenerateCall) MaxResults(maxResults int64) *ReportsGenerateCall 
 
 // Metric sets the optional parameter "metric": Numeric columns to
 // include in the report.
-func (c *ReportsGenerateCall) Metric(metric string) *ReportsGenerateCall {
+func (c *ReportsGenerateCall) Metric(metric []string) *ReportsGenerateCall {
 	c.opt_["metric"] = metric
 	return c
 }
@@ -4428,7 +4436,7 @@ func (c *ReportsGenerateCall) Metric(metric string) *ReportsGenerateCall {
 // metric to sort the resulting report on, optionally prefixed with "+"
 // to sort ascending or "-" to sort descending. If no prefix is
 // specified, the column is sorted ascending.
-func (c *ReportsGenerateCall) Sort(sort string) *ReportsGenerateCall {
+func (c *ReportsGenerateCall) Sort(sort []string) *ReportsGenerateCall {
 	c.opt_["sort"] = sort
 	return c
 }
@@ -4464,16 +4472,22 @@ func (c *ReportsGenerateCall) Do() (*AdsenseReportsGenerateResponse, error) {
 	params.Set("endDate", fmt.Sprintf("%v", c.endDate))
 	params.Set("startDate", fmt.Sprintf("%v", c.startDate))
 	if v, ok := c.opt_["accountId"]; ok {
-		params.Set("accountId", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("accountId", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["currency"]; ok {
 		params.Set("currency", fmt.Sprintf("%v", v))
 	}
 	if v, ok := c.opt_["dimension"]; ok {
-		params.Set("dimension", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("dimension", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["filter"]; ok {
-		params.Set("filter", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("filter", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["locale"]; ok {
 		params.Set("locale", fmt.Sprintf("%v", v))
@@ -4482,10 +4496,14 @@ func (c *ReportsGenerateCall) Do() (*AdsenseReportsGenerateResponse, error) {
 		params.Set("maxResults", fmt.Sprintf("%v", v))
 	}
 	if v, ok := c.opt_["metric"]; ok {
-		params.Set("metric", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("metric", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["sort"]; ok {
-		params.Set("sort", fmt.Sprintf("%v", v))
+		for _, d := range v.([]string) {
+			params.Add("sort", fmt.Sprintf("%v", d))
+		}
 	}
 	if v, ok := c.opt_["startIndex"]; ok {
 		params.Set("startIndex", fmt.Sprintf("%v", v))
